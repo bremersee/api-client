b336420c1f325a3d717325d3f052b2cad576fb14
Jenkins <jenkins@jenkins.bremersee.lokal>
2022-03-11 18:37:39 +0100

    Site checkin for project Api Client Parent Module

---
+++ api-client-webflux-spring-boot-autoconfigure/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.bremersee.apiclient.webflux.spring.boot.autoconfigure"}]
---
+++ api-client-webflux-spring/testapidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.bremersee.apiclient.webflux"},{"l":"org.bremersee.apiclient.webflux.app"},{"l":"org.bremersee.apiclient.webflux.contract"},{"l":"org.bremersee.apiclient.webflux.contract.spring"},{"l":"org.bremersee.apiclient.webflux.contract.spring.multipart"}]
